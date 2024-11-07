ed95bb12ea4df5aceb3f78e4a50ce2a9f90f607e
peninahd <peninah.deichman@post.runi.ac.il>
2024-11-07 18:21:07 +0200

    fixed code for Bill3 assignment

--- Bill3.java
+++ Bill3.java
@@ -19,7 +19,7 @@ public class Bill3 {
 				double rounded = Math.ceil(split);
 			
 		//tell the user how much each diner owes
-		System.out.println("Pay " + rounded + " shekels each.");
+		System.out.println("Dear " + name1 + ", " + name2 + ", and " + name3 + ": " + "pay " + rounded + " Shekels each.");
   
 	}
 }
