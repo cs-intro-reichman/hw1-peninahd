db32e3287357849be4d571f7483daf45e4d6a820
peninahd <peninah.deichman@post.runi.ac.il>
2024-11-07 21:07:58 +0200

    fixed the thousands bug

--- NumWords.java
+++ NumWords.java
@@ -1,23 +1,35 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    // receives the number input as a string
 		int number = Integer.parseInt(args[0]);
 
-		//takes the last digit from the number
+		//takes the last number from the integer
 		int ones = number % 10;
+		
 		//this deletes the last number so now we are left with the reminaing
 		number = number / 10;
+		
 		//repeat the process for tens
 		int tens = number % 10;
-
 		number = number / 10;
-		//takes the third digit 
+
+		//hundreds
 		int hundreds = number % 10;
+		number = number / 10;
 
-		//the program will stop here and not read any other digits if the number is bigger thn 999
+		//thousands
+		int thousands = number;
 
-	System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
+		//convert hundreds and thousands into strings
+		String hundredsString = String.valueOf(hundreds);
+		String thousandsString = "";
+		if (thousands != 0) {
+		
+			thousandsString = String.valueOf(thousands);
+		}
+
+	System.out.println((thousandsString + hundredsString) + " hundreds, " + tens + " tens, and " + ones + " ones.");
 
 	}
 }
